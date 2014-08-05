c8c04bc0c73fd367c3e8b3abbd04cf9ebd6584fb
diosmosis <benakamoorthi@fastmail.fm>
2014-08-06 01:49:30 +0200

    Disabling other tests and debugging travis.

--- tests/travis/autoupdate_travis_yml.sh
+++ tests/travis/autoupdate_travis_yml.sh
@@ -5,6 +5,7 @@
 
 # only run auto-update for first travis job and if not a pull request
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_JOB_NUMBER" == *.1 ]]; then
+    echo "cmd: $GENERATE_TRAVIS_YML_COMMAND\n";
     $GENERATE_TRAVIS_YML_COMMAND > generated.travis.yml
     cat generated.travis.yml
     echo ""
